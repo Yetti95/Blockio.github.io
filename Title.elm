67ae48054bfeecb28813c366e8361e59e979db73
Yetti95 <Yetti95@gmail.com>
2018-05-02 17:58:49 -0600

    Blockio

---
+++ Title.elm
@@ -0,0 +1,29 @@
+import Html exposing (Html, text, program)
+import Keyboard
+import Blockio
+
+subscriptions : model -> Sub Msg
+
+subscriptions model =
+  Sub.batch
+  [
+  Keyboard.downs KeyMsg
+  ]
+
+type Msg = KeyMsg Keyboard.KeyCode
+
+view : model -> Html Msg
+view model = text "Press any key to continue"
+--  div []
+--    [ [ text "BLOCKIO"]
+--    , div [] [text "press enter"]
+--    , []
+--    ]
+
+update msg model =
+  case msg of
+     KeyMsg code ->
+        Blockio.main
+
+main =
+    program {view = view, update = update, subscriptions = subscriptions, init = ( model, Cmd.none )}
